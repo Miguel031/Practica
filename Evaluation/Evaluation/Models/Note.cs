320d11b1483c39067a9644163bb3e58a003b5cd9
visual_community@outlook.com <visual_community@outlook.com>
2019-09-27 16:47:38 -0400

    Clases en web

---
+++ Evaluation/Evaluation/Models/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Evaluation.Models
+{
+    public enum TypeStatus { Aprovedd, Reprobated }
+    public class Note
+    {
+        [Key]
+        public string Subject { get; set; }
+
+        public int Prom { get; set; }
+
+        public TypeStatus Status { get; set; }
+
+    }
+}
---
+++ Evaluation/Evaluation/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Evaluation.Models
+{
+    public enum TypeGender { Male, Female, Other }
+
+    public class Student
+    {
+        [Key]
+        public string Name { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)] 
+        public DateTime Birthdate { get; set; }
+        
+        [Required]
+        public TypeGender Gender { get; set; }
+    }
+}
